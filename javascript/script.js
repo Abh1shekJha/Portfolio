cff29eb9e1a83a360c868ea0a5e85e442717aee9
Abhishek Jha <abhishekjhacse47@gmail.com>
2021-07-01 00:08:04 +0530

    project initiated

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+//typing animation script
+var Typed =  new Typed(".typing", {
+   strings: ["Programmer", "Student", "Web Developer", "Cybersecurity Enthusiast", "Poet"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
